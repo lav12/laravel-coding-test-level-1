328dc19164f01471f5ab0bf4a3d3d5db68ca74e6
Lava <danny_lavan@yahoo.com>
2022-06-23 17:33:09 +0800

    update route prefix

--- events/routes/api.php
+++ events/routes/api.php
@@ -17,11 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\AuthController;
 
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::post('/v1/login', [AuthController::class, 'login']);
-
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::group([
+    "prefix" => "/v1",
+], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 
 
